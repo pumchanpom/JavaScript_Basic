47f78d33039043e33f3ab05c989dfc5ce22a1fae
pumchanpom <pum.chanpom@gmail.com>
2023-03-10 10:39:49 +0700

    Add description and correct code.

--- raceDay.js
+++ raceDay.js
@@ -1,3 +1,15 @@
+// Race Day Project
+// Race number:
+// Early adults receive a race number at or above 1000.
+// All others receive a number below 1000.
+
+// Start time:
+// Adult registrants run at 9:30 am or 11:00 am.
+// Early adults run at 9:30 am.
+// Late adults run at 11:00 am.
+// Youth registrants run at 12:30 pm (regardless of registration).
+
+
 let raceNumber = Math.floor(Math.random() * 1000);
 
 let isEarly = false;
@@ -10,7 +22,6 @@ if (runnerAge > 18 && isEarly === true) {
     raceNumber += 1000;
     console.log(`${raceNumber}: 11:00 AM`);
 } else if (runnerAge < 18) {
-    raceNumber += 1000;
     console.log(`${raceNumber}: 12:30 PM`);
 } else {
     console.log('Please see the registration desk.')
